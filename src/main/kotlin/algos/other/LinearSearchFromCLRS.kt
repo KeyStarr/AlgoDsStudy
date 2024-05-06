9cab889517f2321efd567fe74a6306fb2773931f
kirillstarostin <cyrilstarostin@gmail.com>
2024-05-06 18:49:58 +0300

    Fix the loop invariant for linear search

--- src/main/kotlin/algos/other/LinearSearchFromCLRS.kt
+++ src/main/kotlin/algos/other/LinearSearchFromCLRS.kt
@@ -3,13 +3,14 @@ package algos.other
 class LinearSearchFromCLRS {
 
     /**
-     * Loop invariant: starting from i=1, numbers[0:i-1] does not contain a number equal to x;
-     *  - init: skip the i=0, correct and does not leave out first array element (it would be checked on i=1);
-     *  - maintenance: correct for each iteration;
+     * Loop invariant: at the start of each iteration numbers[0:i-1] does not contain a number equal to x
+     * (interpret numbers[0:-1] as an empty subarray);
+     *  - init: empty subarray, correct;
+     *  - maintenance: correct before each subsequent iteration;
      *  - termination:
-     *      - if the element equal to x is found, the loop is interrupted and `i` is returned;
+     *      - if the element equal to x is found, the loop is interrupted and the correct index is returned;
      *      - otherwise when i=numbers.size the loop is terminated and that always means that numbers[0:numbers.size-1]
-     *          does not contain an element equal to x. Thus, `null` is returned.
+     *          (the entire array) does not contain an element equal to x. Thus, `null` is returned.
      */
     operator fun invoke(numbers: IntArray, x: Int): Int? {
         for (i in numbers.indices) if (numbers[i] == x) return i
