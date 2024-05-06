369ac3b8e05bb49a16ab96ea395ed2b921f2776b
kirillstarostin <cyrilstarostin@gmail.com>
2024-05-06 18:49:40 +0300

    Implement space-efficient selection sort

--- src/main/kotlin/algos/sort/SelectionSort.kt
+++ src/main/kotlin/algos/sort/SelectionSort.kt
@@ -7,8 +7,8 @@ class SelectionSort {
     // time: O(n^2) selection sort
     // space: O(3n) => O(n)
     // can't use it for this problem cause its required to be nlogn and checked by time limit, but just to practice
-    // that particular alrogithm!
-    operator fun invoke(nums: IntArray): IntArray {
+    // that particular algorithm.
+    fun naive(nums: IntArray): IntArray {
         val takenPositions = BooleanArray(nums.size)
         val newNums = IntArray(nums.size)
 
@@ -33,10 +33,25 @@ class SelectionSort {
         return newNums
     }
 
+    // came back to selection sort, now studying CLRS Chapter 2.
+    // idea: search the min in the subarray of numbers[i:numbers.size-2], swap numbers[i] and numbers[minInd]
+    // worst case time O(n^2), memory O(1)
+    fun spaceEfficient(numbers: IntArray): IntArray {
+        for (i in 0 until numbers.size - 1) {
+            var minInd = i
+            for (j in (i + 1) until numbers.size) {
+                if (numbers[j] < numbers[minInd]) minInd = j
+            }
 
+            val buff = numbers[i]
+            numbers[i] = numbers[minInd]
+            numbers[minInd] = buff
+        }
+        return numbers
+    }
 }
 
-fun main(){
-    val result = SelectionSort()(intArrayOf(-1, 2, 0, 99, -99))
-    println(result.joinToString(separator = ", "))
+fun main() {
+    val result = SelectionSort().spaceEfficient(intArrayOf(-1, 2, 0, 99, -99))
+    println(result.contentToString())
 }
