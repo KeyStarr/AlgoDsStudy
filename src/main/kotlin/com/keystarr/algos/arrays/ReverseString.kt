d754e5bc9e58b98bd770d01b3b1d6a8289ee583e
kirillstarostin <cyrilstarostin@gmail.com>
2024-05-14 15:40:40 +0300

    LC-344: implement an efficient solution

---
+++ src/main/kotlin/com/keystarr/algos/arrays/ReverseString.kt
@@ -0,0 +1,40 @@
+package com.keystarr.algos.arrays
+
+/**
+ * LC-344 https://leetcode.com/problems/reverse-string/description/
+ * constraints:
+ *  • 1 <= s.length <= 10^5;
+ *  • s\[i] is ASCII;
+ *  • space: O(1);
+ *  • no explicit time.
+ *
+ *  Final note - done an efficient solution myself in 5 mins.
+ */
+class ReverseString {
+
+    /**
+     * Tools: Two Pointers.
+     * Idea: swap 2 elements each iteration, starting from the edges.
+     *
+     * Time: always O(n)
+     * Space: always O(1)
+     */
+    fun efficient(input: CharArray) {
+        var left = 0
+        var right = input.size - 1
+        while (left < right) {
+            val buff = input[left]
+            input[left] = input[right]
+            input[right] = buff
+
+            left++
+            right--
+        }
+    }
+}
+
+fun main() {
+    val input = "a12hf0".toCharArray()
+    ReverseString().efficient(input)
+    println(input)
+}
