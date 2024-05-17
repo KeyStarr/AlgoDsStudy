5ee085f93314d3b5b8a349e829e19c2b2140e6ed
kirillstarostin <cyrilstarostin@gmail.com>
2024-05-17 14:22:40 +0300

    LC-2351: implement an efficient solution; leave a todo for HashSet implementation

---
+++ src/main/kotlin/com/keystarr/algorithm/hashtable/FirstLetterToAppearTwice.kt
@@ -0,0 +1,28 @@
+package com.keystarr.algorithm.hashtable
+
+/**
+ * LC-2351 https://leetcode.com/problems/first-letter-to-appear-twice/description/
+ * difficulty: easy
+ * constraints:
+ *  • 2 <= s.length <= 100;
+ *  • s consists of lowercase English letters;
+ *  • s has at least one repeated letter;
+ *  • no explicit time/space.
+ */
+class FirstLetterToAppearTwice {
+
+    /**
+     * Set<Char>, cause we need to find the 1st one to be twice.
+     *
+     * Time: O(n)
+     * Space: O(1) cause only 26 chars in alphabet.
+     */
+    fun solution(input: String): Char {
+        val occurredLettersSet = mutableSetOf<Char>()
+        input.forEach {
+            if (occurredLettersSet.contains(it)) return it
+            occurredLettersSet.add(it)
+        }
+        throw IllegalStateException("The result is guaranteed, shouldn't happen.")
+    }
+}
---
+++ src/main/kotlin/com/keystarr/datastructure/set/HashSet.kt
@@ -0,0 +1,5 @@
+package com.keystarr.datastructure.set
+
+class HashSet {
+    // TODO()
+}
