d2159b7c3f1b9cd55d3351f40ba1c94632196641
kirillstarostin <cyrilstarostin@gmail.com>
2024-06-06 13:32:16 +0300

    LC-112: implement an arguably cleaner solution

--- src/main/kotlin/com/keystarr/algorithm/graph/tree/binary/PathSum.kt
+++ src/main/kotlin/com/keystarr/algorithm/graph/tree/binary/PathSum.kt
@@ -43,12 +43,32 @@ class PathSum {
             if (isMatch) return true
         }
         if (node.right != null) {
-            val isMatch =  internalRecursive(node.right!!, targetSum, currentSum)
+            val isMatch = internalRecursive(node.right!!, targetSum, currentSum)
             return isMatch
         }
 
         // left != null but no a match + right == null OR left == null and right != null but not a match
         return false
+    }
+
+    /**
+     * Same core idea and complexities as [efficient] but:
+     *  - use only 1 variable for sum instead of passing both target and currentSum.
+     *      Intuition - if pass currentSum=targetSum to root, and then at each node we subtract node's value from currentSum,
+     *       then at the leaves, if current branch's sum equals targetSum, currentSum would be 0;
+     *  - don't use local variables to check for whether the left or the right subtrees lead to the target branch.
+     */
+    fun efficientCleaner(root: IntTreeNode?, targetSum: Int): Boolean =
+        efficientCleanerRecursive(node = root, remainderAfterParent = targetSum)
+
+    private fun efficientCleanerRecursive(node: IntTreeNode?, remainderAfterParent: Int): Boolean =
+        when {
+            node == null -> false // base case #1, either root or the non-existing node after a leaf
+            node.left == null && node.right == null -> remainderAfterParent - node.value == 0 // base case #2, leaf
+            else -> { // recursive case
+                val currentRemainder = remainderAfterParent - node.value
+                efficientCleanerRecursive(node.left, currentRemainder) || efficientCleanerRecursive(node.right, currentRemainder)
+        }
     }
 }
 
