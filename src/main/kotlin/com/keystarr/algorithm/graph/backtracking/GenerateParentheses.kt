50ffd831188717a9df225f10d88d011b07569d1a
kirillstarostin <cyrilstarostin@gmail.com>
2024-07-21 19:18:00 +0300

    LC-22: correct time estimation

--- src/main/kotlin/com/keystarr/algorithm/graph/backtracking/GenerateParentheses.kt
+++ src/main/kotlin/com/keystarr/algorithm/graph/backtracking/GenerateParentheses.kt
@@ -9,9 +9,14 @@ package com.keystarr.algorithm.graph.backtracking
  * Final notes:
  *  • cool, backtracking pattern as-is yet again. It kills generation problems, at least some;
  *  • here pruning = generation of next nodes in the decision tree was the biggest part for adaption;
+ *  • wow, I actually got an approximate time estimation right. For the first time for backtracking! Reasoning from the
+ *   decision tree's height and neighbors count at each node seems to really work for the approximate at least, which
+ *   should be fine for interviews.
  *
  * Value gained:
- *  • practiced backtracking on all combinations generation problem.
+ *  • practiced backtracking on all combinations generation problem;
+ *  • apparently really approximate time estimation might be ok, like I (surprisingly) actually did here by reasoning
+ *   from the decision tree's height and its total nodes / paths to leaves counts.
  */
 class GenerateParentheses {
 
@@ -27,18 +32,20 @@ class GenerateParentheses {
      *  - n == 1 => just a single combination, correct;
      *  - n == 2 => 2, correct.
      *
-     * Time: average/worst O(2^n)
+     * Time: average/worst O(n*2^n) // TODO: estimate precisely. What are these Catalon numbers and when are they useful?
      *  - since each a valid pair of parenthesis consist of exactly 2 elements, each combination will have the length
      *   of exactly n*2 => decision tree height is exactly n*2 with all laves at the last level
      *  - at each node we'll have at most 2 next nodes, but sometimes just 1 if we used up all opening brackets,
      *   so average/worst = 2^(n*2) = O(2^n)
+     *  - and, finally, to add the combination to results we have to create a String instance from the StringBuilder's array
+     *   which takes O(n) time
      * Space: O(n) if we don't include the results space
      *  - height of the callstack is n*2
      *  - results size worst/average O(2^n), but actually less
      */
-    fun solution(n: Int) = mutableListOf<String>().apply {
+    fun efficient(n: Int) = mutableListOf<String>().apply {
         backtrack(
-            current = "",
+            current = StringBuilder(),
             openedBrackets = 0,
             closedBrackets = 0,
             n = n,
@@ -47,22 +54,30 @@ class GenerateParentheses {
     }
 
     private fun backtrack(
-        current: String,
+        current: StringBuilder,
         openedBrackets: Int,
         closedBrackets: Int,
         n: Int,
         results: MutableList<String>,
     ) {
         if (openedBrackets == n && closedBrackets == n) {
-            results.add(current)
+            results.add(current.toString())
             return
         }
 
         if (openedBrackets - closedBrackets > 0) {
-            backtrack("$current)", openedBrackets, closedBrackets + 1, n, results)
+            current.append(')')
+            backtrack(current, openedBrackets, closedBrackets + 1, n, results)
+            current.deleteLast()
         }
         if (openedBrackets != n) {
-            backtrack("$current(", openedBrackets + 1, closedBrackets, n, results)
+            current.append('(')
+            backtrack(current, openedBrackets + 1, closedBrackets, n, results)
+            current.deleteLast()
         }
+    }
+
+    private fun StringBuilder.deleteLast() {
+        deleteAt(length - 1)
     }
 }
