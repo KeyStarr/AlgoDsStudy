5599463824842e0266eae8e63d2fff94e0660bcc
kirillstarostin <cyrilstarostin@gmail.com>
2024-07-21 21:12:07 +0300

    LC-967: simplify and optimize the solution

--- src/main/kotlin/com/keystarr/algorithm/graph/backtracking/NumbersWithSameConsecutiveDifferences.kt
+++ src/main/kotlin/com/keystarr/algorithm/graph/backtracking/NumbersWithSameConsecutiveDifferences.kt
@@ -1,16 +1,32 @@
 package com.keystarr.algorithm.graph.backtracking
 
 /**
- *
+ * LC-967 https://leetcode.com/problems/numbers-with-same-consecutive-differences/description/
+ * difficulty: medium
  * constraints:
- *  - 2 <= n <= 9
- *  - 0 <= k <= 9
+ *  • 2 <= n <= 9
+ *  • 0 <= k <= 9
  *
  * Final notes:
- *  - MISSED a GLARING edge case!
+ *  • MISSED a GLARING edge case
+ *  • interesting, so, constructing the number from left to right (higher to lower digits) was much easier than from
+ *   right to left, AND we could just store the current intermediate number as Int, no lists required => optimal solution!
+ *   we could use Int for lower-to-higher digits construction too, but we'd need 10^currentDigitInd or to carry over
+ *   current positionMultiplier and do positionMultiplier*10 each iteration (have it as an arg to recursion)
+ *   -----
+ *   TL;DR; for number generation consider going from higher-to-lower digits and simply currentNumber*10+newDigit to add
+ *   the new digit. Plus using only Int as the intermediate number representation
+ *  • wow, I ACTUALLY got the time estimate right! The 2nd time for backtracking!!! Reasoned from the tree height and then
+ *   the max neighbors each node may have. It seems to follow a pattern, the upper time boundary for this types of problems,
+ *   which is perfect for a quick under-the-stress big O (not theta obviously, theta time seems to be much harder and
+ *   unrealistic during an interview).
  *
  * Value gained:
- *  -
+ *  • practiced backtracking for integer numbers generation => learnt to try starting from higher to lower digits and
+ *   using just a number for an intermediate representation (at least it MAY be easier than starting from lower to higher
+ *   generally, that's a hypothesis;
+ *  • practiced estimating backtracking algo time => seems to be that the workToCollectCombination * heightOfTree^maxNeighborsForEachNode
+ *   formula kinda works, or at least these focal points have proven so far to be good starting points for time estimation.
  */
 class NumbersWithSameConsecutiveDifferences {
 
@@ -43,66 +59,64 @@ class NumbersWithSameConsecutiveDifferences {
      *  - k == 0 => a combination can have only 1 digit => consider only 1 direction then, not both to avoid duplicates;
      *  - since n > 1 => its always safe to use 0 as the first digit.
      *
-     * Time: O([numberLength]*2^[numberLength])
+     * Time: O(2^[numberLength])
      *  - the height of the decision tree is always [numberLength]
      *  - from one node in worst case we have 2 children => binary tree (but it can be 1 or 0 children as well, and
      *   leaves are not all on the same level)
-     *  - when we reach the leaf, we copy the `current` array and add it into results => that takes O([numberLength]) time
+     *  - when we arrive at the full valid combination, it's already a number, so we just insert it into the results ArrayList
+     *   which takes amortized O(1) time.
      * Space: O([numberLength]) for the decision tree height, if we don't count results space
      *
      * ----
      * could be optimized for absDiff==0 case such that we won't remove from the currentNumber
      */
-    fun solution(numberLength: Int, absDiff: Int): IntArray = mutableListOf<Int>().apply {
-        (0..9).forEach { firstDigit ->
+    fun efficient(numberLength: Int, absDiff: Int): IntArray = mutableListOf<Int>().apply {
+        (1..9).forEach { firstDigit ->
             backtrack(
-                currentNumber = mutableListOf(firstDigit),
-                targetDigits = numberLength,
+                currentNumber = firstDigit,
+                digitsLeftCount = numberLength - 1,
                 absDiff = absDiff,
                 results = this,
             )
         }
     }.toIntArray()
 
+    /**
+     * construct [currentNumber] left-to-right (starting from higher order digits to lower)
+     */
     private fun backtrack(
-        currentNumber: MutableList<Int>,
-        targetDigits: Int,
+        currentNumber: Int,
+        digitsLeftCount: Int,
         absDiff: Int,
         results: MutableList<Int>,
     ) {
-        if (currentNumber.size == targetDigits) {
-            results.add(currentNumber.toNumber())
+        if (digitsLeftCount == 0) {
+            results.add(currentNumber)
             return
         }
 
+        val lastDigit = currentNumber % 10
         directions.forEachIndexed { ind, direction ->
             if (ind == 1 && absDiff == 0) return@forEachIndexed
 
-            val newDigit = currentNumber.last() + direction * absDiff
-            if (newDigit in 1..9 || (newDigit == 0 && currentNumber.size != targetDigits - 1)) {
-                currentNumber.add(newDigit)
-                backtrack(currentNumber, targetDigits, absDiff, results)
-                currentNumber.removeLast()
-            }
-        }
-    }
+            val newDigit = lastDigit + direction * absDiff
+            if (newDigit !in 0..9) return@forEachIndexed
 
-    private fun List<Int>.toNumber(): Int {
-        var positionMultiplier = 1
-        var result = 0
-        for (digit in this) {
-            result += digit * positionMultiplier
-            positionMultiplier *= 10
+            backtrack(
+                currentNumber = currentNumber * 10 + newDigit,
+                digitsLeftCount = digitsLeftCount - 1,
+                absDiff = absDiff,
+                results = results,
+            )
         }
-        return result
     }
 }
 
 fun main() {
     println(
-        NumbersWithSameConsecutiveDifferences().solution(
-            numberLength = 2,
-            absDiff = 0,
+        NumbersWithSameConsecutiveDifferences().efficient(
+            numberLength = 3,
+            absDiff = 7,
         ).contentToString()
     )
 }
