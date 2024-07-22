7340158d31d16815ad10dfaad9b28480fc3f3a69
kirillstarostin <cyrilstarostin@gmail.com>
2024-07-22 16:10:32 +0300

    LC-2418: add the problem for future practicing

---
+++ src/main/kotlin/com/keystarr/algorithm/graph/tree/treemap/SortThePeople.kt
@@ -0,0 +1,9 @@
+package com.keystarr.algorithm.graph.tree.treemap
+
+/**
+ * LC-2418 https://leetcode.com/problems/sort-the-people/description/?envType=daily-question&envId=2024-07-22
+ */
+class SortThePeople {
+
+    // TODO: solve (and understand deeply) via the TreeMap!
+}
