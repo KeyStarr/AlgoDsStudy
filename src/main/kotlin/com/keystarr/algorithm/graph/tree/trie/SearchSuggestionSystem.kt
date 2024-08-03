43e91532bd8b1252c942ed23037f1f0e16dcf9c9
kirillstarostin <cyrilstarostin@gmail.com>
2024-08-03 23:08:10 +0300

    LC-1268: implement a better solution with just top k heap

--- src/main/kotlin/com/keystarr/algorithm/graph/tree/trie/SearchSuggestionSystem.kt
+++ src/main/kotlin/com/keystarr/algorithm/graph/tree/trie/SearchSuggestionSystem.kt
@@ -21,17 +21,55 @@ import kotlin.math.min
  *  - 1st submit fail => missed an obvious case with a word's length being less than searchWord's length, a smart optimization
  *   I did for building a trie 🙂;
  *  - 2nd submit fail => missed an obvious edge case, after Xth char no common prefix for the searchWord in words
- *   => per the problem statement, add emptyList() to the results.
+ *   => per the problem statement, add emptyList() to the results;
+ *  - 🔥 [justTopK] makes so much sense here, totally no need for a Trie, even though initially this problem looks like a perfect
+ *   candidate for a Trie o_O. So weird the course's author picked it, why not pick some other problem which actually showcases
+ *   a must-have Trie for an optimal solution??
  *
  * Value gained:
  *  - 🏆 first problem I've ever solved with a Trie, wo-hoo!
  *  - practiced using a Trie with pre-processing, data at each node being an actual result, should that node be required
- *   in the final answer.
+ *   in the final answer;
+ *  - proved that this problem actually doesn't need a Trie for an efficient solution via [justTopK].
  */
 class SearchSuggestionSystem {
 
-    // TODO: understand and implement just the heap solution https://leetcode.com/problems/search-suggestions-system/solutions/471718/java-priority-queue-no-sort-or-trie/
-    //  (or the one in the top bars of time)
+    /**
+     * Notice that with the [trie] the only actual advantage that we have from that data structure is that, when collecting
+     *  results, we trivially don't go along words which at some point had chars not equal to corresponding target chars
+     *  => we can simply do that with a boolean array, marking all words which failed matching
+     *  => simply iterate along the rest, use the same logic with a maxHeap for topk min words.
+     *
+     * Time: O(n*k)
+     *  - outer loop always takes k=searchWord.length iterations;
+     *   - inner loops always takes n=words.length iterations = O(n*(m+logm)) = O(n);
+     *    - clear the heap O(m)
+     *    - add/remove from the heap O(logm)
+     *    here m is a const, always m=3
+     *
+     * Space: O(n)
+     *  - unfit O(n)
+     *  - don't count results, and result takes O(m) space, which is a const O(3) in this problem
+     */
+    fun justTopK(words: Array<String>, searchWord: String): List<List<String>> {
+        val results = mutableListOf<List<String>>()
+        val result = PriorityQueue<String> { o1, o2 -> o2.compareTo(o1) } // for top k mins
+        val unfit = BooleanArray(size = words.size)
+        searchWord.forEachIndexed { charInd, targetChar ->
+            result.clear()
+            for (wordInd in words.indices) {
+                val word = words[wordInd]
+                if (unfit[wordInd] || charInd >= word.length || word[charInd] != targetChar) {
+                    unfit[wordInd] = true
+                    continue
+                }
+                result.add(word)
+                if (result.size > 3) result.remove()
+            }
+            results.add(result.sorted())
+        }
+        return results
+    }
 
     /**
      * find words with common prefix to target across multiple options => try a Trie (:D)
@@ -54,15 +92,16 @@ class SearchSuggestionSystem {
      *  - there are no strings in [products] that have any common prefix with [searchWord] => a special case of the case
      *   above => correct as-is.
      *
-     * Time: always O(n*k+g)
-     *  - building a trie O(n*k*logm)=O(n*k), where n=words.length, k=average word length, m=max matches constraint, here its const 3
-     *   (logm for adding into the heap)
-     *  - main loop O(g*m*logm) = O(g) with m being at most 3, where g=searchWord.length
-     * Space: average/worst O(n*k)
-     *  - total characters count O(n*k)
+     * Time: always O(n*k+k) = O(n*k)
+     *  - building a trie O(n*k*logm)=O(n*k), where n=words.length, k=searchWord.length, m=max matches constraint, here its const 3
+     *   - logm for adding/removing from the heap
+     *  - main loop O(k*m*logm) = O(k) with m being at most 3
+     * Space:
+     *  - we create trie nodes only for chars that match for the given position the actual search word chars,
+     *   and only up to searchWord.length => at most O(
      */
     fun trie(products: Array<String>, searchWord: String): List<List<String>> {
-        val root = products.buildTrie(maxChars = searchWord.length)
+        val root = products.buildTrie(searchWord)
         var currentNode: TrieNode? = root
         val results = mutableListOf<List<String>>()
         searchWord.forEach { currentChar ->
@@ -72,11 +111,13 @@ class SearchSuggestionSystem {
         return results
     }
 
-    private fun Array<String>.buildTrie(maxChars: Int) = TrieNode().also { root ->
+    private fun Array<String>.buildTrie(searchWord: String) = TrieNode().also { root ->
         forEach { word ->
             var currentNode = root
-            for (i in 0 until min(word.length, maxChars)) {
+            for (i in 0 until min(word.length, searchWord.length)) {
                 val char = word[i]
+                if (char != searchWord[i]) return@forEach
+
                 if (currentNode.children[char] == null) currentNode.children[char] = TrieNode()
                 currentNode = currentNode.children.getValue(char)
 
@@ -119,9 +160,9 @@ private data class TrieNode(
 
 fun main() {
     println(
-        SearchSuggestionSystem().trie(
-            products = arrayOf("bags", "baggage", "banner", "box", "cloths"),
-            searchWord = "bags",
+        SearchSuggestionSystem().justTopK(
+            words = arrayOf("havana"),
+            searchWord = "tatiana",
         )
     )
 }
