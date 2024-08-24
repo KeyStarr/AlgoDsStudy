885530f5c57ed905ee46bea8128bdf70477ec192
kirillstarostin <cyrilstarostin@gmail.com>
2024-08-25 00:07:55 +0300

    LC-1325: implement a cleaner solution

--- src/main/kotlin/com/keystarr/algorithm/graph/tree/binary/DeleteLeavesWithAGivenValue.kt
+++ src/main/kotlin/com/keystarr/algorithm/graph/tree/binary/DeleteLeavesWithAGivenValue.kt
@@ -12,7 +12,14 @@ package com.keystarr.algorithm.graph.tree.binary
  *   • caught quickly the CORE observation: we must consider the node for removal only after both its left and right
  *    subtrees have been processed, cause node itself after these might become a leaf => post-order dfs.
  *   • caught and both the only edge case, for that we might delete the root as well if it becomes a leaf, but we need
- *    to handle that in a special way.
+ *    to handle that in a special way;
+ *   • ⚠️ still failed to see the cleanest version [dfsCleaner], though didnt really try to, after doing [efficient].
+ *   💡🔥I start to notice that basically the cleanest binary tree solution is (potentially) always working only with
+ *   the current subtree rooted at [root] and its children, nothing else, clearly unlike [efficient], which works with
+ *   children of the children => requires a special edge case handling for root.
+ *   💡 Cleanliness of a BT problem solution seems to correlate well with LinkedList problems - one might do an efficient
+ *    solution, but if its not utilizing some property (only 2 pointers for LL, only root and its direct children for BT)
+ *    => its gonna be a little dirty, not the most succinct version.
  *
  * Value gained:
  *  • practiced solving a binary tree problem efficiently using in-order/post-order dfs;
@@ -21,6 +28,8 @@ package com.keystarr.algorithm.graph.tree.binary
  *   would be nothing to draw on). And I need to practice that specifically, and on different types of inputs/algos 💡
  */
 class DeleteLeavesWithAGivenValue {
+
+    // TODO: did alright, but optionally retry in 1-2 weeks
 
     /**
      * post-order DFS? post-order for removal condition to trigger after all updates to both left and right subtrees.
@@ -64,4 +73,18 @@ class DeleteLeavesWithAGivenValue {
     }
 
     private fun IntBinaryTreeNode.isLeaf() = left == null && right == null
+
+    /**
+     * Goal - remove all leaves equal to target repeatedly until none are left in the binary tree [root],
+     *  return the root after the transformations.
+     *
+     * Clean post-order dfs.
+     *
+     * Learned from https://leetcode.com/problems/delete-leaves-with-a-given-value/solutions/484264/java-c-python-recursion-solution/
+     */
+    fun dfsCleaner(root: IntBinaryTreeNode, target: Int): IntBinaryTreeNode? {
+        root.left = root.left?.let { dfsCleaner(it, target) }
+        root.right = root.right?.let { dfsCleaner(it, target) }
+        return if (root.left == null && root.right == null && root.`val` == target) null else root
+    }
 }
