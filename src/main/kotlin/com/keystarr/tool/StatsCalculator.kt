0b5b589a0daa13bfc5ec543a45829d6a861ad128
kirillstarostin <cyrilstarostin@gmail.com>
2024-08-25 02:07:13 +0300

    Leave a stub to possibly create a stats tool

---
+++ src/main/kotlin/com/keystarr/tool/StatsCalculator.kt
@@ -0,0 +1,15 @@
+package com.keystarr.tool
+
+class StatsCalculator {
+
+    /**
+     * idea: return the number of problems of each type of difficulty solved for each pattern
+     *  - take difficulty from the problem class doc comment "difficulty :";
+     *  - pattern = name of the package
+     *
+     * => print the prettified stats to console for now, check if that's useful/fun
+     */
+    operator fun invoke() {
+        TODO()
+    }
+}
