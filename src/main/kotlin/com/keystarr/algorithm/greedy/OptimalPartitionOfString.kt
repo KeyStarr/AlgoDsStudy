405c2729f96af4d6eee7cbd0b54e3f6dce9f517f
kirillstarostin <cyrilstarostin@gmail.com>
2024-09-07 00:51:37 +0300

    LC-2405: implement a bit manipulation solution with an even better const

--- src/main/kotlin/com/keystarr/algorithm/greedy/OptimalPartitionOfString.kt
+++ src/main/kotlin/com/keystarr/algorithm/greedy/OptimalPartitionOfString.kt
@@ -1,6 +1,7 @@
 package com.keystarr.algorithm.greedy
 
 /**
+ * ⭐️ a beautiful bit manipulation for existence checking example
  * LC-2405 https://leetcode.com/problems/optimal-partition-of-string/description/
  * difficulty: medium (imho, leet-easy)
  * constraints:
@@ -17,7 +18,10 @@ package com.keystarr.algorithm.greedy
  *   not considering what comes next => it is greedy! Just quite an unusual setting for it.
  *
  * Value gained:
- *  • practiced solving a string modification type problem using greedy and a hashset/counting array.
+ *  • practiced solving a string modification type problem using greedy and a hashset/counting array;
+ *  • uh-huh! so an even more efficient approach to keep track of which values were met, if the amount of distinct values
+ *   is quite narrow (<32,64) is to not even use an array, but simply an integer as the container! Where ith bit is whether
+ *   we've encountered the (initialOffset + i) character.
  */
 class OptimalPartitionOfString {
 
@@ -65,4 +69,37 @@ class OptimalPartitionOfString {
         }
         return partitionsCount + 1
     }
+
+    /**
+     * Huh, since we only got up to 26 distinct chars consecutively ordered by their codes after some offset => try
+     *  using a single integer variable as a container, where each ith bit, if set, means that ('a'+i) char was already added
+     *  to the current partition.
+     *
+     * An even better time+space const then [array], but same asymptotic complexities as both previous solutions.
+     *
+     * -------
+     *
+     * Learned about that angle thanks to https://leetcode.com/problems/optimal-partition-of-string/solutions/3376693/image-explanation-3-approaches-o-1-space-c-java-python/
+     * just that the bitwise approach is possible here => done the rest myself.
+     */
+    fun bitManipulation(text: String): Int {
+        var currentPartition = 0
+        var partitionsCount = 0
+        text.forEach { char ->
+            val offset = char - 'a'
+            val keyBit = 1 shl offset
+            if ((currentPartition and keyBit) != 0) {
+                currentPartition = 0
+                partitionsCount++
+            }
+            currentPartition = currentPartition or keyBit
+        }
+        return partitionsCount + 1
+    }
+}
+
+fun main() {
+    println(
+        OptimalPartitionOfString().bitManipulation("ssssss")
+    )
 }
